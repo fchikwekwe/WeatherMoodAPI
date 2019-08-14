2599d3ff855bcb8f0087da639d9b162ea7ff8f11
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-08-14 11:44:34 -0700

    🌟 big refactor to simplify routes, get rid of database and make sure project requirements are fullfilled

--- controllers/moods.js
+++ controllers/moods.js
@@ -1,79 +1,41 @@
 const Mood = require('../models/mood.js');
 
 const axios = require('axios');
+const JSON = require('circular-json');
 
 const apiKey = process.env.WEATHER_API_KEY;
 
 module.exports = (app) => {
-    // ROOT (SHOW all moods)
-    app.get('/moods', (req, res) => {
-        Mood.find()
-            .then(moods => {
-                res.json(moods);
-            })
-            .catch(error => {
-                console.log(error);
-            })
+    // CREATE one weather object based on the user's location
+    app.post('/weather', async (req, res) => {
+        let city = req.query.city;
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+        const weather = await axios.post(url);
+        var weatherObj = [
+            {
+                city: city, 
+                weather: weather.data.main
+            }
+        ]
+        res.send(weatherObj);
     })
 
-    // CREATE one mood
-    app.post('/moods/new', async (req, res) => {
-        // console.log(req.query);
+    // CREATE one mood object and send back to user
+    app.post('/mood', async (req, res) => {
+        // Get mood and city via post request from the user
         let city = req.query.city;
         let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-
+        // Use axios to ping the API and return a weather object
         const weather = await axios.post(url);
-
         var mood = [
             {
                 mood: req.query.mood,
                 city: city,
-                weather: weather
+                weather: weather.data.main //Just the important stuff
             }
         ]
-        console.log("something")
-        let save = await saveMood(mood);
-
-        console.log('redirecting');
-        // return res.redirect('/moods');
-    })
-
-    function saveMood(mood) {
-        const moodDoc = Mood.create(mood);
-        return moodDoc
-    }
-
-    // SHOW one mood
-    app.get('/moods/:id', (req, res) => {
-        Mood.findById(req.params.id)
-            .then(mood => {
-                res.json(mood);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    })
-
-    // UPDATE one mood
-    app.put('/moods/:id', (req, res) => {
-        Mood.findByIdAndUpdate(req.params.id, req.body)
-            .then(mood => {
-                res.redirect(`/moods/:id`);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    })
-
-    // DELETE one mood
-    app.delete('/moods/:id', (req, res) => {
-        Mood.findByIdAndRemove(req.params.id)
-            .then(mood => {
-                res.redirect('/moods');
-            })
-            .catch(error => {
-                console.log(error);
-            })
+        // Send the mood object back to the user
+        res.send(mood);
     })
 }
